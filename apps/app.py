ac734e7dc76c03bb82bccfa8f02c6aed46e8c689
Indrajit <indrajit@MacBook-Air-de-Indrajit.local>
2026-04-10 22:27:40 +0200

    Create initial commit for the application part

---
+++ apps/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template(
+        "home.html",
+        date=datetime.now()
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ apps/template.py/home.py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mon site Flask</title>
+</head>
+<body>
+
+    <h1>🚀 Hello DevOps</h1>
+
+    <p>Ceci est mon premier site Flask simple</p>
+
+    <p>📅 Date actuelle : {{ date }}</p>
+
+</body>
+</html>
