5d3340bd07647aedc48e3035dc7de94c2fa7f1f2
Indrajit RAJASOORIAR <indrajitrajasooriar@hotmail.fr>
2026-04-13 20:47:05 +0200

    Fix script.sh issues & correct issues from ami

--- script.sh
+++ script.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 # Update system
-apt-get update -y
+dnf upgrade -y
 
 # Install Docker
-apt-get install -y docker.io
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
 # Enable Docker service
 systemctl enable docker
 systemctl start docker
+echo "DOCKER STARTED"
+
+# Install SSM Agent
+dnf install -y https://s3.amazonaws.com/amazon-ssm-us-east-1/latest/linux_amd64/amazon-ssm-agent.rpm
+
+# Start SSM Agent
+systemctl start amazon-ssm-agent
+systemctl enable amazon-ssm-agent
+
+echo "SSM INSTALL DONE"
 
 # Add ubuntu user to docker group
 usermod -aG docker ubuntu
@@ -18,4 +30,6 @@ sleep 10
 
 # Run container (à adapter)
 docker run -d -p 5000:5000 python:3.11 python -m http.server 5000
+
+
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
   description = "The AMI ID to use for the EC2 instance"
   type        = string
-  default     = "ami-056244ee7f6e2feb8"
+  default     = "ami-0956e0658ee3b1ffb"
 }
 
 variable "instance_type" {
