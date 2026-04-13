88c18e64d9113b6999d0a27f39ff9882e37f9651
Indrajit RAJASOORIAR <indrajitrajasooriar@hotmail.fr>
2026-04-13 23:23:25 +0200

    Fix script user-data for ec2

--- script.sh
+++ script.sh
@@ -3,12 +3,14 @@
 # Update system
 dnf upgrade -y
 
+# Install Docker repo
+dnf install -y dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
 # Install Docker
+dnf install -y docker-ce docker-ce-cli containerd.io
 
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-
-# Enable Docker service
+# Start Docker
 systemctl enable docker
 systemctl start docker
 echo "DOCKER STARTED"
@@ -19,17 +21,13 @@ dnf install -y https://s3.amazonaws.com/amazon-ssm-us-east-1/latest/linux_amd64/
 # Start SSM Agent
 systemctl start amazon-ssm-agent
 systemctl enable amazon-ssm-agent
-
 echo "SSM INSTALL DONE"
 
-# Add ubuntu user to docker group
-usermod -aG docker ubuntu
+# Add ec2-user to docker group
+usermod -aG docker ec2-user
 
-# Wait Docker to be ready
+# Wait Docker
 sleep 10
 
-# Run container (à adapter)
+# Run container
 docker run -d -p 5000:5000 python:3.11 python -m http.server 5000
-
-
-
