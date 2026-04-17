865679313a99c2d9c805f4dbdea59a860466962e
Indrajit RAJASOORIAR <indrajitrajasooriar@hotmail.fr>
2026-04-17 09:12:50 +0200

    Fix Dockerfile issues & change ami to Amazon linux 2023

--- script-start.sh
+++ script-start.sh
@@ -1,8 +1,66 @@
 #!/bin/bash
 set -e
 
-systemctl enable --now docker
+echo "Updating system..."
+sudo dnf update -y
 
-sleep 3
+# -------------------------
+# Docker install (AL2023)
+# -------------------------
+echo "Installing Docker..."
 
-docker run -d -p 5000:5000 python:3.11 python -m http.server 5000
+sudo dnf install -y docker
+
+# Enable and start Docker
+sudo systemctl enable docker
+sudo systemctl start docker
+
+echo "DOCKER STARTED"
+docker --version
+
+sleep 10
+
+# Add ec2-user to docker group
+sudo usermod -aG docker ec2-user
+
+# -------------------------
+# SSM Agent (AL2023 way)
+# -------------------------
+echo "Installing SSM Agent..."
+
+sudo dnf install -y amazon-ssm-agent
+
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+
+echo "SSM INSTALL DONE"
+
+echo "Setup completed"
+
+# -------------------------
+# Installing git
+# -------------------------
+
+echo "Installing git..."
+sudo dnf install -y git
+
+# -------------------------
+# Clone the repository
+# -------------------------
+
+echo "Cloning the repository..."
+cd /opt
+git clone https://github.com/IndrajitRajasooriar/python-aws.git
+cd python-aws
+
+# -------------------------
+# Docker build & run
+# -------------------------
+
+echo "Building the Docker image..."
+sudo docker build -t flask-app .
+
+sudo docker run -d -p 5000:5000 flask-app
+
+echo "Checking logs..."
+sudo docker logs $(sudo docker ps -q) || true
